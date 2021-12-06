ea78f4c2f1691953daddab5e938836598808a4a4
liamrahav <im@liamrahav.me>
2021-12-06 12:55:13 -0500

    set up mandeR

--- dataset/shapefiles.py
+++ dataset/shapefiles.py
@@ -2,23 +2,33 @@
 Download & unzip shapefiles from the United States Congressional District Shapefiles
 dataset provided by UCLA. Downloads from the 93rd Congress onwards
 """
+import atexit
 from pathlib import Path
 import urllib.request
 from zipfile import ZipFile
 
+import mander
 from tqdm import trange
 
 BASE_URL = "https://cdmaps.polisci.ucla.edu/shp/districts{:03d}.zip"
-BASE_OUTPUT_DIR = "dataset/raw/shapefiles/{}"
+BASE_OUTPUT_DIR = "dataset/raw/shapefiles/"
 
-if __name__ == "__main__":
+
+def download_shapefiles(output_dir) -> None:
     for i in trange(93, 114):
         # yields a path to a temporary file, cleaned up at end of script
         path, _ = urllib.request.urlretrieve(BASE_URL.format(i))
 
         with ZipFile(path, "r") as zip_file:
-            directory = BASE_OUTPUT_DIR.format(i)
+            if output_dir[-1] != "/":
+                output_dir += "/"
+            directory = "{}{}".format(output_dir, i)
             Path(directory).mkdir(parents=True, exist_ok=True)
             zip_file.extractall(path=directory)
 
-        urllib.request.urlcleanup()
+    urllib.request.urlcleanup()
+
+
+if __name__ == "__main__":
+    atexit.register(urllib.request.urlcleanup)
+    download_shapefiles(BASE_OUTPUT_DIR)
---
+++ install_manderR.R
@@ -0,0 +1,3 @@
+wd="<FILL_IN>/districts-and-race/"
+setwd(wd)
+install.packages("mander.tar.gz", repos = NULL, type = "source")
