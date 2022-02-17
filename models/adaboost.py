812c50736a68f34c014276d7fc2f9896916323a1
liamrahav <im@liamrahav.me>
2022-02-16 23:35:30 -0500

    architecture done, adaboost dt and dt

--- models/adaboost.py
+++ models/adaboost.py
@@ -1,19 +1,22 @@
 import os
-import matplotlib.pyplot as plt
 import pprint
 
 import joblib
 import numpy as np
 import pandas as pd
-import pydotplus
 import sklearn
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import GridSearchCV
 
 from utils import get_dataset, REDUCED_FEATURES, FEATURES, LABELS, PRETRAINED_PREFIX
 
-SAVE_MODEL_NAME = "dt"
-PARAM_GRID = {"max_depth": [2, 4, 6, 10, 15]}
+
+SAVE_MODEL_NAME = "adaboost_dt"
+PARAM_GRID = {
+    "base_estimator__max_depth": [1, 2, 3, 4, 5, 6],
+    "n_estimators": [200, 300, 400],
+}
 
 
 def perform_grid_search(dataset="full", verbose=True, save=True, load=True):
@@ -24,9 +27,9 @@ def perform_grid_search(dataset="full", verbose=True, save=True, load=True):
         except:
             pass
 
-    X, y, cv = get_dataset(features="reduced")
+    X, y, cv = get_dataset(features=dataset)
 
-    clf = DecisionTreeClassifier(random_state=0)
+    clf = AdaBoostClassifier(DecisionTreeClassifier(random_state=0), random_state=0)
     gs = GridSearchCV(
         clf,
         param_grid=PARAM_GRID,
@@ -47,14 +50,4 @@ def perform_grid_search(dataset="full", verbose=True, save=True, load=True):
 
 
 if __name__ == "__main__":
-    clf = perform_grid_search(dataset='reduced')
-    dot_file = "figures/tree_reduced_dataset.dot"
-    export_graphviz(
-        clf,
-        dot_file,
-        rounded=True,
-        filled=True,
-        class_names=["white"] + LABELS,
-        feature_names=REDUCED_FEATURES,
-    )
-    pydotplus.graph_from_dot_file(dot_file).write_png(dot_file.replace(".dot", ".png"))
+    clf = perform_grid_search(dataset="full")
--- models/utils.py
+++ models/utils.py
@@ -1,4 +1,8 @@
+import pandas as pd
+import numpy as np
+
 DATASET_PATH = "dataset/preprocessed/dataset.pickle.bz2"
+PRETRAINED_PREFIX = 'pretrained/'
 
 REDUCED_FEATURES = [
     "year",
@@ -21,3 +25,46 @@ LABELS = [
     "black",
     "hispanic",
 ]
+
+
+def get_dataset(features='full'):
+    if features not in ('full', 'reduced'):
+        raise ValueError('features must be "full" or "reduced".')
+
+    featnames = FEATURES if features == 'full' else REDUCED_FEATURES
+
+    dataset = pd.read_pickle(DATASET_PATH)
+
+    X = dataset[featnames].to_numpy()
+
+    # We know that black is the first column and hispanic is second
+    # To get labels, simply turn all hispanic from 1 to 2 via multiplication
+    # Then merge all the labels. This yields: other = 0, black = 1, hispanic = 2
+    y = dataset[LABELS].to_numpy()
+    y[:,1] *= 2
+    y = y.sum(axis=1)
+
+    # We notice 4 columns where race label is NaN. Let's just strike those out
+    to_del = (np.argwhere(np.isnan(y)).T)[0]
+    X = np.delete(X, to_del, axis=0)
+    y = np.delete(y, to_del, axis=0)
+    assert X.shape[0] == y.shape[0]
+
+    # Set up initial split as follows:
+    # TRAIN: 1976               TEST: 1978
+    # TRAIN: 1976, 1978         TEST: 1980
+    # ...
+    # TRAIN: 1976, ..., 2010    TEST: 2012
+    groups = []
+    for year in list(dataset.year.unique()):
+        groups.append(np.where(X[:,0] == year)[0].tolist())
+
+    cv = []
+    for i in range(len(groups) - 1):
+        subgroup = []
+        for j in range(i + 1):
+            subgroup += list(groups[j])
+
+        cv.append((subgroup, groups[i + 1]))
+
+    return X, y, cv
